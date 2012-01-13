9d51f7e38b0323ecc616b886272430b4825abdae
Noel Cower <ncower@gmail.com>
2012-01-12 17:23:39 -0800

    Whitespace fixes

--- lib/oaktree.rb
+++ lib/oaktree.rb
@@ -3,20 +3,21 @@ require 'fileutils'
 
 # The enormous blog class
 class OakTree
+
   def initialize spec
     @spec = spec
-    
+
     @posts = []
-    
+
     sync_posts
   end
 
   def blogspec
     @spec
   end
-  
+
   private
-  
+
   def sync_posts
     entries = Dir.glob("#{@spec.blog_root}/source/**/*.md")
     entries.each { |entry|
@@ -27,7 +28,7 @@ class OakTree
       left.time <=> right.time
     }
   end
-  
+
 end
 
 require 'oaktree/specification'
