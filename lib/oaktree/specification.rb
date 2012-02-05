29af4b482a20955ede25e4d806931fc558e46bf0
Noel Cower <ncower@gmail.com>
2012-02-05 16:48:20 -0700

    Reorganizing stuff a bit.

--- lib/oaktree/specification.rb
+++ lib/oaktree/specification.rb
@@ -1,97 +1,98 @@
 require 'date'
 
-class OakTree; end
+class OakTree
 
-# Specifications for the blog, operates similar to Gem::Specification.
-# URLs and paths are strings and should not end in a slash.
-class OakTree::Specification
-  @@KEY_VALUE_PATTERN = /^\s*(?<key>[\w_]+)\s*:\s*(?<value>.*?)\s*(#|$)/
+  # Specifications for the blog, operates similar to Gem::Specification.
+  # URLs and paths are strings and should not end in a slash.
+  class Specification
+    @@KEY_VALUE_PATTERN = /^\s*(?<key>[\w_]+)\s*:\s*(?<value>.*?)\s*(#|$)/
   
-  # The blog's title
-  attr_accessor :title
-  # A description of the blog
-  attr_accessor :description
-  # The base URL of the blog (i.e., http://localhost/blog) - should not have a
-  # trailing slash.
-  attr_accessor :base_url
-  # The post path (i.e., the subdirectory where posts are stored).
-  attr_accessor :post_path
-  # The blog root, where files are stored locally.
-  # Beneath this directory, there should be /source and /public directories,
-  # where post sources and the blog output, respectively, are stored. If these
-  # don't exist, they'll be created when generating the blog.
-  # This cannot be changed.
-  attr_reader :blog_root
-  # The name of the blog's author (currently assumes a single author)
-  attr_accessor :author
-  # The number of posts displayed per page
-  attr_accessor :posts_per_page
+    # The blog's title
+    attr_accessor :title
+    # A description of the blog
+    attr_accessor :description
+    # The base URL of the blog (i.e., http://localhost/blog) - should not have a
+    # trailing slash.
+    attr_accessor :base_url
+    # The post path (i.e., the subdirectory where posts are stored).
+    attr_accessor :post_path
+    # The blog root, where files are stored locally.
+    # Beneath this directory, there should be /source and /public directories,
+    # where post sources and the blog output, respectively, are stored. If these
+    # don't exist, they'll be created when generating the blog.
+    # This cannot be changed.
+    attr_reader :blog_root
+    # The name of the blog's author (currently assumes a single author)
+    attr_accessor :author
+    # The number of posts displayed per page
+    attr_accessor :posts_per_page
   
-  # Loads a specification from a file.
-  def self.from_file(path)
-    raise "Spec file does not exist" unless File.exists? path
+    # Loads a specification from a file.
+    def self.from_file(path)
+      raise "Spec file does not exist" unless File.exists? path
     
-    self.new { |spec|
+      self.new { |spec|
       
-      File.open(path, 'r') { |io|
+        File.open(path, 'r') { |io|
         
-        io.each_line { |line|
-          line = line.strip
+          io.each_line { |line|
+            line = line.strip
           
-          next if line.empty? || line.start_with?('#')
+            next if line.empty? || line.start_with?('#')
           
-          match = line.match @@KEY_VALUE_PATTERN
+            match = line.match @@KEY_VALUE_PATTERN
           
-          if match.nil? then
-            puts "Invalid entry in blog_spec: #{line}"
-            next
-          end
+            if match.nil? then
+              puts "Invalid entry in blog_spec: #{line}"
+              next
+            end
           
-          key = match[:key]
-          value = match[:value]
+            key = match[:key]
+            value = match[:value]
           
-          case key.to_sym
-            when :title
-              spec.title = value
-            when :description
-              spec.description = value
-            when :base_url
-              spec.base_url = value
-            when :post_path
-              spec.post_path = value
-            when :author
-              spec.author = value
-            when :posts_per_page
-              spec.posts_per_page = value.to_i
-            else
-              puts "Invalid name for entry in blog_spec: #{line}"
-          end
+            case key.to_sym
+              when :title
+                spec.title = value
+              when :description
+                spec.description = value
+              when :base_url
+                spec.base_url = value
+              when :post_path
+                spec.post_path = value
+              when :author
+                spec.author = value
+              when :posts_per_page
+                spec.posts_per_page = value.to_i
+              else
+                puts "Invalid name for entry in blog_spec: #{line}"
+            end
+          }
+        
         }
-        
+      
+        Dir.chdir(File.dirname(path))
+      
       }
-      
-      Dir.chdir(File.dirname(path))
-      
-    }
-  end
+    end
   
-  # Initializes the Specification with its default values.
-  def initialize
-    # initialize default values for most properties
-    self.title = 'Title'
-    self.description = 'A brief description of this blog'
-    self.base_url = 'http://localhost'
-    self.post_path = 'post'
-    self.author = "Someone"
-    self.posts_per_page = 10
+    # Initializes the Specification with its default values.
+    def initialize
+      # initialize default values for most properties
+      self.title = 'Title'
+      self.description = 'A brief description of this blog'
+      self.base_url = 'http://localhost'
+      self.post_path = '/post'
+      self.author = "Someone"
+      self.posts_per_page = 10
     
-    yield self if block_given?
+      yield self if block_given?
     
-    @blog_root = File.expand_path Dir.getwd
-  end
+      @blog_root = File.expand_path Dir.getwd
+    
+    end
   
-  def export_string
-    <<-EOT
+    def export_string
+      <<-EOT
 # metadata
 title:  #{@title}
 description: #{@description}
@@ -103,6 +104,9 @@ base_url:  #{@base_url}
 
 # public content paths
 post_path: #{@post_path}
-    EOT
-  end
-end
+      EOT
+    end
+    
+  end # Specification
+
+end # OakTree
