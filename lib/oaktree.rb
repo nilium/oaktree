43f54d5fe3959b9afe83b986263d7bd14e190d57
Noel Cower <ncower@gmail.com>
2012-02-05 17:04:38 -0700

    Added the generate method to OakTree - it just writes the HTML files for the blog and makes a few quick checks to see if it can skip generating some pages.

--- lib/oaktree.rb
+++ lib/oaktree.rb
@@ -1,5 +1,6 @@
 require 'date'
 require 'fileutils'
+require 'pathname'
 
 require 'oaktree/specification'
 require 'oaktree/post_data'
@@ -28,6 +29,51 @@ class OakTree
   def posts
     sync_posts
     return @posts
+  end
+  
+  # Generates and writes the HTML files for the blog.
+  # If force_rebuild is false, it will only rebuild pages with posts that have
+  # changed, otherwise it rebuilds everything.
+  # This does not check for changes to templates or other content -- if you
+  # make any changes to templates, you must force a rebuild.
+  def generate force_build = false
+    blog_template = Template::Blog.new self
+    
+    blog_template.modes.each {
+      |mode|
+      
+      blog_template.mode = mode
+      
+      (1..blog_template.pages).each { |page|
+        blog_template.page = page
+        path = blog_template.local_path
+        pretty_path = Pathname.new(path).relative_path_from(Pathname.new(@spec.blog_root)).to_s
+        
+        mtime = File.exists?(path) ? File.mtime(path) : nil
+        needs_update = force_build || mtime.nil?
+        
+        if ! needs_update
+          needs_update = blog_template.posts.inject(false) { |memo, post|
+            data = post.post_data
+            memo || mtime < data.file_mtime
+          }
+          
+          if ! needs_update
+            puts "Skipping #{pretty_path}"
+            next
+          end
+        end
+        
+        dir = File.dirname(path)
+        FileUtils.mkdir_p dir unless File.directory? dir
+        
+        puts "Writing #{pretty_path}"
+        File.open(path, 'w') {
+          |io|
+          io.write blog_template.render
+        }
+      }
+    }
   end
 
   private
