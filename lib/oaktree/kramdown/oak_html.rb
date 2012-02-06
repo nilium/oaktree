7aaf407fd2a3a73037f1a04f28816766e0e14720
Noel Cower <ncower@gmail.com>
2012-02-06 00:43:54 -0700

    Added convert_div and a new footnote_content implementation to go back to using rev="footnote" in footnotes.  This makes the W3C unhappy, but it keeps with the existing conventions I'm aware of.

--- lib/oaktree/kramdown/oak_html.rb
+++ lib/oaktree/kramdown/oak_html.rb
@@ -1,4 +1,5 @@
 require 'kramdown'
+require 'kramdown/element'
 
 class OakTree
   
@@ -11,6 +12,33 @@ class OakTree
           el.options[:name] = @options[:auto_id_prefix] + el.options[:name]
         end
         super
+      end
+      
+      def convert_div el, indent
+        "<#{el.type}#{html_attributes el.attr}>\n#{inner el, indent+1}\n</#{el.type}>"
+      end
+      
+      def footnote_content
+        return '' if @footnotes.empty?
+        
+        block = ::Kramdown::Element.new(:div, nil, {'class' => 'footnotes'})
+        block.children << (list = ::Kramdown::Element.new(:ol))
+        
+        @footnotes.each { |fn_name, fn_elem|
+          item = ::Kramdown::Element.new(:li, nil, {'id' => "fn:#{fn_name}"})
+          # because we'll end up manipulating a child, we may as well do a deep copy
+          item.children = Marshal.load(Marshal.dump(fn_elem.children))
+          
+          # basically what kramdown already does here
+          last = (last = item.children.last).type == :p ? last : (::Kramdown::Element.new(:p))
+          # yes, I'm using rev, shut up
+          last.children << (anchor = ::Kramdown::Element.new(:a, nil, {'href' => "#fnref:#{fn_name}", 'rev' => 'footnote'}))
+          anchor.children << ::Kramdown::Element.new(:raw, '&#8617;')
+
+          list.children << item
+        }
+        
+        convert(block, 2)
       end
       
     end
