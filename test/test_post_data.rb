431d2111d23b7d45524e3056a084c14bf726617b
Noel Cower <ncower@gmail.com>
2012-02-05 17:14:51 -0700

    Fixed up the post data test.

--- test/test_post_data.rb
+++ test/test_post_data.rb
@@ -5,8 +5,6 @@ POST_CONTENT_FIRST = <<EOT
 title: first
 time: 2000-01-01 00:00:00 +0700
 link: http://localhost
-tags: first, second, third
-categories: birds, fish
 ---
 
 content one
@@ -17,8 +15,6 @@ POST_CONTENT_SECOND = <<EOT
 title: second
 time: 2001-01-01 00:00:00 +0700
 link: http://127.0.0.1
-tags: fourth, fifth, sixth
-categories: dogs, cats
 ---
 
 content two
@@ -34,12 +30,10 @@ class PostTest < Test::Unit::TestCase
         io.write POST_CONTENT_FIRST
       }
 
-      post = OakTree::Post.new tempfile
+      post = OakTree::PostData.new tempfile
 
       assert_equal 'first', post.title
       assert_equal 'http://localhost', post.link
-      assert_equal ['first', 'second', 'third'], post.tags
-      assert_equal ['birds', 'fish'], post.categories
       assert_equal DateTime.parse('2000-01-01 00:00:00 +0700'), post.time
 
       File.open(tempfile, 'w') { |io|
@@ -48,8 +42,6 @@ class PostTest < Test::Unit::TestCase
 
       assert_equal 'second', post.title
       assert_equal 'http://127.0.0.1', post.link
-      assert_equal ['fourth', 'fifth', 'sixth'], post.tags
-      assert_equal ['dogs', 'cats'], post.categories
       assert_equal DateTime.parse('2001-01-01 00:00:00 +0700'), post.time
 
     ensure
