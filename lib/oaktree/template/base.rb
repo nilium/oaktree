edbd6646f731d627834adad8aa5cbb2b5eccd76c
Noel Cower <me@spifftastic.net>
2012-08-08 02:43:58 -0700

    Whitespace fixes.

--- lib/oaktree/template/base.rb
+++ lib/oaktree/template/base.rb
@@ -5,18 +5,18 @@ require 'oaktree/template'
 class OakTree
 
   module Template
-  
+
     class Base < Mustache
       DEFAULT_DATETIME_FORMAT = '%-d %B %Y @ %l:%M %p'
-    
+
       def proc_for_datetime time
         proc { |format|
           format ||= DEFAULT_DATETIME_FORMAT
-        
+
           render(time.strftime(format))
         }
       end
-      
+
       self.template_path = 'template'
 
     end # Base
