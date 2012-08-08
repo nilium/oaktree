7b23ccf8700c921b00f5a56e026eee62f3a2799d
Noel Cower <me@spifftastic.net>
2012-08-07 18:28:22 -0700

    Increment version to 0.4.0.

--- lib/oaktree.rb
+++ lib/oaktree.rb
@@ -12,7 +12,7 @@ require 'oaktree/template'
 # The central blog class (also just called a tree from time to time)
 class OakTree
 
-  VERSION = '0.3.1'
+  VERSION = '0.4.0'
 
   @@EMPTY_DIR_ENTRIES = ['.', '..']
 
